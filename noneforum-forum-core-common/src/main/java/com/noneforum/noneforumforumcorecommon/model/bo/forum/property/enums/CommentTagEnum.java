b5a42222642b6d87326a4f8b707508b75f400720
li-ruida <2605209238@qq.com>
2023-09-26 00:36:37 +0800

    论坛基础属性建设 0.0.2

--- noneforum-forum-core-common/src/main/java/com/noneforum/noneforumforumcorecommon/model/bo/forum/property/enums/CommentTagEnum.java
+++ noneforum-forum-core-common/src/main/java/com/noneforum/noneforumforumcorecommon/model/bo/forum/property/enums/CommentTagEnum.java
@@ -1,9 +1,59 @@
 package com.noneforum.noneforumforumcorecommon.model.bo.forum.property.enums;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * @author lrd
  * @date 2023-09-25 23:51
  */
 public enum CommentTagEnum {
-    //TODO:评论tag
+    UNKNOW(0, "未知标签", "未知标签");
+    private Integer code;
+    private String name;
+    private String desc;
+
+    CommentTagEnum() {
+    }
+
+    CommentTagEnum(Integer code, String name, String desc) {
+        this.code = code;
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public long getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    private static final Map<String, CommentTagEnum> MAPPING;
+
+    static {
+        MAPPING = new HashMap<>(values().length, 1);
+        for (CommentTagEnum value : values()) {
+            MAPPING.put(value.getName(), value);
+        }
+    }
+
+    public static CommentTagEnum of(Integer code) {
+        for (CommentTagEnum commentTagEnum : CommentTagEnum.values()) {
+            if (Objects.equals(commentTagEnum.getCode(), code)) {
+                return commentTagEnum;
+            }
+        }
+        return CommentTagEnum.UNKNOW;
+    }
+
+    public static CommentTagEnum of(String name) {
+        return MAPPING.getOrDefault(name, CommentTagEnum.UNKNOW);
+    }
 }
--- noneforum-forum-core-common/src/main/java/com/noneforum/noneforumforumcorecommon/model/bo/forum/property/enums/PostTagEnum.java
+++ noneforum-forum-core-common/src/main/java/com/noneforum/noneforumforumcorecommon/model/bo/forum/property/enums/PostTagEnum.java
@@ -1,9 +1,59 @@
 package com.noneforum.noneforumforumcorecommon.model.bo.forum.property.enums;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * @author lrd
  * @date 2023-09-24 21:41
  */
 public enum PostTagEnum {
-    //TODO:帖子标签枚举
+    UNKNOW(0, "未知标签", "未知标签");
+    private Integer code;
+    private String name;
+    private String desc;
+
+    PostTagEnum() {
+    }
+
+    PostTagEnum(Integer code, String name, String desc) {
+        this.code = code;
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public long getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    private static final Map<String, PostTagEnum> MAPPING;
+
+    static {
+        MAPPING = new HashMap<>(values().length, 1);
+        for (PostTagEnum value : values()) {
+            MAPPING.put(value.getName(), value);
+        }
+    }
+
+    public static PostTagEnum of(Integer code) {
+        for (PostTagEnum postTagEnum : PostTagEnum.values()) {
+            if (Objects.equals(postTagEnum.getCode(), code)) {
+                return postTagEnum;
+            }
+        }
+        return PostTagEnum.UNKNOW;
+    }
+
+    public static PostTagEnum of(String name) {
+        return MAPPING.getOrDefault(name, PostTagEnum.UNKNOW);
+    }
 }
