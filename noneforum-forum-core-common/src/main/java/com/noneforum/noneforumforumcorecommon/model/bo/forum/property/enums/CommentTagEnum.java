4c7aa573ee1e08905ee2c5199e22f86ef35019c0
li-ruida <2605209238@qq.com>
2023-09-26 00:01:06 +0800

    论坛基础属性建设 0.0.2

--- noneforum-forum-core-common/src/main/java/com/noneforum/noneforumforumcorecommon/model/bo/forum/property/enums/CommentTagEnum.java
+++ noneforum-forum-core-common/src/main/java/com/noneforum/noneforumforumcorecommon/model/bo/forum/property/enums/CommentTagEnum.java
@@ -4,6 +4,6 @@ package com.noneforum.noneforumforumcorecommon.model.bo.forum.property.enums;
  * @author lrd
  * @date 2023-09-25 23:51
  */
-public class CommentTagEnum {
+public enum CommentTagEnum {
     //TODO:评论tag
 }
